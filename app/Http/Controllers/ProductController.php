a77c633fa81caf27ce9ca7b118c2f54af163ba22
hadeel habib <hadeelhabib93@gmail.com>
2024-04-02 17:38:19 +0300

    last commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ProductController extends Controller
 {
 
     public function index()
-    {
-        $product=Product::all();
+    { 
+        $product = Product::all();
         return response()->json([
-            'status'=>'succses',
-            'product'=>$product
+            'status' => 'succses',
+            'product' => $product
         ]);
     }
 
@@ -23,83 +25,89 @@ class ProductController extends Controller
     public function store(Request $request)
     {
 
-      
+
         $request->validate(
             [
-                'name'=>'required|string',
-                'price'=>'required|numeric',
+                'name' => 'required|string',
+                'price' => 'required|numeric',
+            ]
+        );
+
+
+        try {
+            DB::beginTransaction();
+            $product = Product::create([
+                'name' => $request->name,
+                'price' => $request->price
+
             ]);
-
-
-            try {
-                DB::beginTransaction();
-                $product=Product::create([
-                    'name'=>$request->name ,
-                    'price'=>$request->price
-    
-                ]);
-                DB::commit();
+            DB::commit();
             return response()->json([
-                'status'=>'succses',
-                'product'=>$product
+                'status' => 'succses',
+                'product' => $product
             ]);
-            } catch (\Throwable $th) {
-                DB::rollBack();
-                Log::error($th);
-                return response()->json([
-                    'status'=>'error',
-                    
-                ]);
-            }
-           
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error($th);
+            return response()->json([
+                'status' => 'error',
 
+            ]);
+        }
     }
 
 
     public function show(Product $product)
     {
         return response()->json([
-            'status'=>'succses',
-            'product'=>$product
+            'status' => 'succses',
+            'product' => $product
         ]);
     }
 
 
     public function update(Request $request, Product $product)
     {
-        
         $request->validate(
             [
                 'name'=>'nullable|string',
                 'price'=>'nullable|numeric',
             ]);
-            $Data=[];
-                if(isset($request->name)){
-                    $Data['name']=$request->name;
-                }
-                if(isset($request->price)){
-                    $Data['price']=$request->price;
-                }
-            
-            $product->update([
-               $Data,
+        $Data = [];
+        try {
+            DB::beginTransaction();
+
+            if (isset($request->name)) {
+                $Data['name'] = $request->name;
+            }
+            if (isset($request->price)) {
+                $Data['price'] = $request->price;
+            }
+
+
+
+            DB::commit();
+            $product->update($Data);
+            return response()->json([
+                'status' => "success",
+                "message" => "product updated successfully"
             ]);
-        return response()->json([
-            'status'=>'succses',
-            'product'=>$product
-        ]);
-
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            log::error($th);
+            return response()->json([
+                'status' => "Failed"
+            ], 500);
+        }
     }
-
 
     public function destroy(Product $product)
     {
-        
+
         $product->delete();
         return response()->json([
-            'status'=>'succses',
-            'product'=>$product
+            'status' => 'succses',
+            'product' => $product
         ]);
-
     }
 }
